86248238912aa0e0dac41268dc9fc58db03a23d0
htien <mac@Macs-MacBook-Pro.local>
2021-09-04 22:22:57 +0700

    change background and add responsive

--- sketch.js
+++ sketch.js
@@ -1,43 +1,27 @@
-let WIDTH;
-let HEIGHT;
-let CLOCK_RADIUS;
-let SECOND_RADIUS;
-let MINUTE_RADIUS;
-let HOUR_RADIUS;
-let centerX;
-let centerY;
+var windowSize = window.innerWidth <  window.innerHeight ?  window.innerWidth :  window.innerHeight;
+const WIDTH = windowSize;
+const HEIGHT =windowSize;
+RADIUS = (HEIGHT * 3) / 5;
+CLOCK_RADIUS = RADIUS * 1.1;
+SECOND_RADIUS = RADIUS * 0.5;
+MINUTE_RADIUS = RADIUS * 0.45;
+HOUR_RADIUS = RADIUS * 0.33;
+centerX = WIDTH / 2;
+centerY = HEIGHT / 2;
 let PI = Math.PI;
-let RADIUS;
-let Digital = document.querySelector("#digital");
 
 let hourHand = 0;
 let minuteHand = 0;
 let secondHand = 0;
 
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
-  WIDTH = windowWidth;
-  HEIGHT = windowHeight;
-  RADIUS = (HEIGHT * 3) / 5;
-  CLOCK_RADIUS = RADIUS * 1.1;
-  SECOND_RADIUS = RADIUS * 0.5;
-  MINUTE_RADIUS = RADIUS * 0.45;
-  HOUR_RADIUS = RADIUS * 0.37;
-  centerX = WIDTH / 2;
-  centerY = HEIGHT / 2;
-}
+let Digital = document.querySelector("#digital");
+
 
 function setup() {
-  WIDTH = windowWidth;
-  HEIGHT = windowHeight;
   canvas = createCanvas(WIDTH, HEIGHT);
-  RADIUS = (HEIGHT * 3) / 5;
-  CLOCK_RADIUS = RADIUS * 1.1;
-  SECOND_RADIUS = RADIUS * 0.5;
-  MINUTE_RADIUS = RADIUS * 0.45;
-  HOUR_RADIUS = RADIUS * 0.37;
-  centerX = WIDTH / 2;
-  centerY = HEIGHT / 2;
+  noStroke();
+  fill(0);
+  circle(centerX, centerY, CLOCK_RADIUS + 2);
 }
 
 function showSecond(i) {
@@ -65,19 +49,21 @@ function showHour(i) {
   var x = Math.cos(i) * HOUR_RADIUS + centerX;
   var y = Math.sin(i) * HOUR_RADIUS + centerY;
   line(centerX, centerY, x, y);
+  
+  circle(centerX, centerY, 4);
+
 }
 
 function draw() {
-  background(0);
 
   noStroke();
   fill(255);
   circle(centerX, centerY, CLOCK_RADIUS);
-  var h = String(hour());
+  var h = hour() >= 10 ? String(hour()) : "0" + String(hour())
   var m = minute() >= 10 ? String(minute()) : "0" + String(minute())
   var s = second() >= 10 ? String(second()) : "0" + String(second());
 
-  Digital.textContent = h + " : " + m + " : " + s;
+  Digital.textContent = `${h} : ${m} : ${s}`;
   secondHand = map(second(), 0, 60, 0, PI * 2) - PI / 2;
   minuteHand = map(minute() + norm(second(), 0, 60), 0, 60, 0, PI * 2) - PI / 2;
   hourHand = map(hour() + norm(minute(), 0, 60), 0, 24, 0, PI * 4) - PI / 2;
